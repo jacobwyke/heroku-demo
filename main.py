df1b70c5abce493c53f820be3401462f405a9d39
Jacob Wyke <jacob@jacobwyke.com>
2019-03-31 17:06:33 +0100

    Tweaking content to first simplify what we have

--- main.py
+++ main.py
@@ -1,28 +1,15 @@
 from flask import Flask, render_template, request
-import iss
 
-app = Flask('MyApp')
 
-@app.route('/')
+app = Flask('cfg')
+
+
+@app.route('/', methods=['GET','POST'])
+def home():
+    return 'Home'
+
+@app.route('/about')
 def hello():
-    return 'Hello World'
-
-@app.route('/iss', methods=['GET','POST'])
-def iss_main():
-    current_location = iss.get_iss_location()
-    pass_time = None
-    postcode = None
-
-    if request.method == 'POST':
-        postcode = request.form.get('postcode')
-        pass_time = iss.get_iss_pass_time_from_postcode(postcode)
-
-    return render_template(
-        'iss.html',
-        current_location=current_location,
-        pass_time=pass_time,
-        postcode=postcode
-    )
-
+    return 'About This Site'
 
 app.run(debug=True)
